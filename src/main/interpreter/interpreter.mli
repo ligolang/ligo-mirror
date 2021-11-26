798db372282d0616e66a8fe06fe40401738c35c7
Suzanne Soy <2923020-ligo.suzanne.soy@users.noreply.gitlab.com>
2021-11-26 06:01:05 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/main/interpreter/interpreter.mli
+++ src/main/interpreter/interpreter.mli
@@ -3,7 +3,8 @@ type interpreter_error = Errors.interpreter_error
 val eval_module : raise:interpreter_error Trace.raise 
     -> steps:int
     -> options:Compiler_options.t
-    -> Ast_typed.module_fully_typed * Tezos_state.context * Ligo_interpreter.Types.env -> Ligo_interpreter.Types.env * Tezos_state.context
+    -> Ast_typed.module_fully_typed * Tezos_state.context * Ligo_interpreter.Types.env
+    -> Ligo_interpreter.Types.env * Tezos_state.context
 val eval_test   : raise:interpreter_error Trace.raise 
     -> steps:int 
     -> options:Compiler_options.t
