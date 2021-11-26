a9fadc33a5ee5fdfa680ed3c36bb690b755e4046
Rémi <lesenechal.remi@gmail.com>
2021-11-26 15:22:28 +0100

    Fix module accessor

--- src/passes/12-aggregation/compiler.ml
+++ src/passes/12-aggregation/compiler.ml
@@ -7,38 +7,29 @@ open Stage_common.Types
   morph modules : Declaration_module -> E_let_in (E_record ..)
 *)
 
-type binder_repr = {
-  var : O.expression_variable ; (* binder given to a module when it is aggregated in a let-in *)
-  label : label                 (* label the module should have if nested into another module *)
-}
+type binder_repr = O.expression_variable (* binder given to a module when it is aggregated in a let-in *)
 
 module Mod_env = struct
   (* env used to remember module types as they are morphed into modules *)
   let variable_of_module_name : loc:Location.t -> string -> O.expression_variable =
     fun ~loc str -> Location.wrap ~loc @@ Var.fresh_like (Var.of_name str)
-  let label_of_module_name : string -> label =
-    fun str -> Label ("m#"^str)
   module ModMap = Map.Make(struct type t = string let compare = String.compare end)
   type t = (O.row_element label_map * binder_repr) ModMap.t
-  let get_ty : t -> string -> O.type_expression = fun env name ->
+  let find : t -> string -> O.type_expression * binder_repr = fun env name ->
     match ModMap.find_opt name env with
-    | Some (content,_) -> O.t_record ~layout:I.default_layout content
-    | None -> failwith (Format.asprintf "no module 2 %s" name)
-  let get_mod_binder : t -> string -> binder_repr = fun env name ->
+    | Some (content,b) -> (O.t_record ~layout:I.default_layout content,b)
+    | None -> failwith (Format.asprintf "TODO: corner case : module '%s' is missing" name)
+  let find_opt : t -> string -> (O.type_expression * binder_repr) option = fun env name ->
     match ModMap.find_opt name env with
-    | Some (_,b) -> b
-    | None -> failwith (Format.asprintf "no module 1 %s" name)
+    | Some (content,b) -> Some (O.t_record ~layout:I.default_layout content,b)
+    | None -> None
   let add ~loc : t -> string * O.type_expression -> t * O.expression_variable =
     fun env (name, t) ->
       let ty = O.get_t_record_exn t in
       let var = variable_of_module_name ~loc name in
-      let label = label_of_module_name name in
-      let binder_repr = {var ; label} in
-      ModMap.add name (ty.content, binder_repr) env, var
+      ModMap.add name (ty.content, var) env, var
   let empty = ModMap.empty
 end
-
-
 
 let rec compile : I.module_fully_typed -> O.expression =
   fun (Module_Fully_Typed module_) -> compile_declaration Mod_env.empty module_
@@ -175,12 +166,19 @@ and compile_expression : Mod_env.t -> I.expression -> O.expression =
       return @@ O.E_constant { cons_name ; arguments }
     )
     | I.E_module_accessor { module_name; element} -> (
-      let ty = Mod_env.get_ty mod_env module_name in
-      let binder = Mod_env.get_mod_binder mod_env module_name in
-      let expr = O.e_a_variable binder.var ty in
-      (* deal with the problem of chained module_accessor .. *)
-      let not_good = Option.value ~default:(Location.wrap @@ Var.of_name "lolilol") @@ O.get_variable (self element) in
-      return @@ O.E_record_accessor {record = expr ; path = Label (Var.to_name not_good.wrap_content)}
+      let rec aux : string List.Ne.t -> I.expression -> string List.Ne.t =
+        fun acc exp ->
+          match exp.expression_content with
+          | E_module_accessor {module_name ; element} ->
+            let acc = Simple_utils.List.Ne.cons module_name acc in
+            aux acc element
+          | E_variable v ->
+            let (name,_int_opt) = Var.internal_get_name_and_counter v.wrap_content in (* feels wrong *)
+            Simple_utils.List.Ne.cons name acc
+          | _ -> failwith "TODO: corner case, not allowed in the syntax"
+      in
+      let path = List.Ne.rev (aux (List.Ne.of_list [module_name]) element) in
+      module_access_to_record_access mod_env path
     )
     | I.E_mod_in { module_binder ; rhs ; let_result } -> (
       let mod_as_record =
@@ -219,20 +217,24 @@ and compile_cases : Mod_env.t -> I.matching_expr -> O.matching_expr =
 
 
 (* morph a module, to rows *)
-and module_to_record : Mod_env.t -> ?acc:(binder_repr * O.expression) list -> I.declaration_loc list -> O.expression =
+and module_to_record : Mod_env.t -> ?acc:(binder_repr * label * O.expression) list -> I.declaration_loc list -> O.expression =
   fun mod_env ?(acc = []) lst ->
     match lst with
     | hd::tl -> (
-      let aggregate ?(new_env = mod_env) ((binder: binder_repr), expr, attr) =
-        let acc = (binder,expr)::acc in
-        O.e_a_let_in binder.var expr (module_to_record ~acc new_env tl) attr
+      let aggregate ?(new_env = mod_env) ((binder,name_opt), expr, attr) =
+        let label = match name_opt with
+          | Some name -> Label name
+          | None -> label_for_constant_decl binder
+        in
+        let acc = (binder,label,expr)::acc in
+        O.e_a_let_in binder expr (module_to_record ~acc new_env tl) attr
       in
       let skip () = module_to_record ~acc mod_env tl in
       match hd.wrap_content with
       | I.Declaration_type _ -> skip ()
-      | I.Declaration_constant { name = _ ; binder ; expr ; attr } -> (
+      | I.Declaration_constant { name ; binder ; expr ; attr } -> (
         let expr = compile_expression mod_env expr in
-        aggregate ({var = binder ; label = label_for_constant_decl binder}, expr, attr)
+        aggregate ((binder,name), expr, attr)
       )
       | I.Declaration_module { module_binder ; module_ ; module_attr } -> (
         let mod_as_record =
@@ -241,41 +243,38 @@ and module_to_record : Mod_env.t -> ?acc:(binder_repr * O.expression) list -> I.
         in
         let attr = known_attributes_for_modules (Some module_attr) in
         let mod_env,binder = Mod_env.add ~loc:hd.location mod_env (module_binder,mod_as_record.type_expression) in
-        let label = (Mod_env.get_mod_binder mod_env module_binder).label in (*REMITODO: ugh*)
-        let binder = { var=binder ; label } in
-        aggregate (binder, mod_as_record, attr) ~new_env:mod_env
+        aggregate ((binder,Some module_binder), mod_as_record, attr) ~new_env:mod_env
       )
       | I.Module_alias { alias ; binders } -> (
         let access = module_access_to_record_access mod_env binders in
         let attr = known_attributes_for_modules None in
         let mod_env,binder = Mod_env.add ~loc:hd.location mod_env (alias, access.type_expression) in
-        let label = (Mod_env.get_mod_binder mod_env alias).label in (*REMITODO: ugh*)
-        let binder = {label ; var = binder} in
-        aggregate (binder, access, attr) ~new_env:mod_env
+        aggregate ((binder,Some alias), access, attr) ~new_env:mod_env
       )
     )
     | [] -> record_of_binders acc
 
 (* morph a module access (A.B.C) to a record access (a.b.c) *)
-and module_access_to_record_access : Mod_env.t -> string Simple_utils.List.Ne.t -> O.expression = fun mod_env (fst,path) ->
+and module_access_to_record_access : Mod_env.t -> string List.Ne.t -> O.expression = fun mod_env (fst,path) ->
   let f : O.expression -> string -> O.expression =
     fun prev module_name ->
-      let {var=_;label} = Mod_env.get_mod_binder mod_env module_name in
-      let ty = Option.value ~default:(failwith "TODO") (O.get_record_field_type prev.type_expression label) in
+      let label = Label module_name in
+      let ty = match O.get_record_field_type prev.type_expression label with
+        | Some ty -> ty | None -> failwith "TODO: corner case, no field x ?"
+      in
       O.e_a_record_access prev label ty
   in
-  (* let init = O.e_a_variable (Mod_env.variable_of_module_name ~loc:Location.generated fst) (Mod_env.get_ty mod_env fst) in *)
-  let init = O.e_a_variable (Mod_env.get_mod_binder mod_env fst).var (Mod_env.get_ty mod_env fst) in
+  let (t,binder) = Mod_env.find mod_env fst in
+  let init = O.e_a_variable binder t in
   List.fold_left path ~f ~init
 
-and record_of_binders : (binder_repr * O.expression) list -> O.expression =
+and record_of_binders : (binder_repr * label *  O.expression) list -> O.expression =
   fun lst ->
     let type_of_rows : O.expression label_map -> O.type_expression =
       fun x -> O.make_t_ez_record (List.map ~f:(fun (O.Label s, v) -> (s, v.type_expression)) (O.LMap.to_kv_list x))
     in
-    let f = fun (k,v : binder_repr * O.expression) ->
-      let {var ; label} = k in
-      let var = O.make_e (O.e_variable var) (v.type_expression) in
+    let f = fun (k,label,e : binder_repr * label * O.expression) ->
+      let var = O.make_e (O.e_variable k) (e.type_expression) in
       (label,var)
     in
     let x = LMap.of_list (List.map lst ~f) in
@@ -292,5 +291,4 @@ and known_attributes_for_modules : I.module_attribute option -> O.known_attribut
 and label_for_constant_decl : O.expression_variable -> label =
   fun var ->
     let (name,_int_opt) = Var.internal_get_name_and_counter var.wrap_content in
-    (* TODO: int_opt ... problem ?*)
     Label name
