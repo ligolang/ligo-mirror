ae3cf05272cfc9e65f1e446cc43cdc187a3980c8
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2021-12-01 14:57:57 -0300

    Add monomorphisation in Of_aggregated.compile_expression

--- src/main/compile/of_aggregated.ml
+++ src/main/compile/of_aggregated.ml
@@ -13,6 +13,7 @@ let compile ~raise : Ast_typed.module_fully_typed -> Mini_c.program = fun p ->
   mini_c *)
 
 let compile_expression ~raise : expression -> Mini_c.expression = fun e ->
+  let e = Self_ast_aggregated.monomorphise_expression e in
   trace ~raise spilling_tracer @@ compile_expression e
 
 let compile_type ~raise : type_expression -> Mini_c.type_expression = fun e ->
